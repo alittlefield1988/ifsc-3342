0e24207b7cb3cc6f8756835e399a4108de9ad951
alittlefield1988 <asmakashova@ualr.edu>
2021-04-20 05:29:31 +0000

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/*function change_vars(){
+  var body = document.getElementsByTagName("body")[0];
+  if ( !body.classList.contains('changed') ) {
+    body.style.setProperty('--color-primary', '#8e44ad');
+    body.style.setProperty('--color-secondary', '#16a085');
+    body.style.setProperty('--color-accent', '#e67e22');
+    body.classList.add('changed');
+  } else {
+    body.style.setProperty('--color-primary', '#2980b9');
+    body.style.setProperty('--color-secondary', '#c0392b');
+    body.style.setProperty('--color-accent', '#16a085');
+    body.classList.remove('changed');
+  }
+}*/
